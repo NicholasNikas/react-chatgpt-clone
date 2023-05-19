be012414163395e178452653e6211c359a508d09
Nicholas Nikas <nnikas@microsoft.com>
2023-05-18 22:09:05 -0400

    Add API configurations to server and client

--- src/App.js
+++ src/App.js
@@ -1,4 +1,52 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        { title: currentTitle, role: "user", content: value },
+        { title: currentTitle, role: message.role, content: message.content },
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -12,12 +60,14 @@ function App() {
         </nav>
       </section>
       <section className="main">
-        <h1>NikasGPT</h1>
+        {!currentTitle && <h1>NikasGPT</h1>}
         <ul className="feed"></ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
           </div>
           <p className="info">
             NikasGPT is a model using ChatGPT which interacts in a
